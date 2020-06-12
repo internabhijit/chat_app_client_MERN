32a9b43509583989bab8696d621ab931f3719c95
internabhijit <intern.abhijit@coutloot.com>
2020-06-12 12:50:49 +0530

    chat room messages api

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -2,14 +2,18 @@ import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
 
-import "./Chat.css";
+import { getFromStorage } from "../../utils/storage";
+
 import InfoBar from "../InfoBar/InfoBar";
 import Input from "../Input/Input";
 import Messages from "../Messages/Messages";
 
+import "./Chat.css";
+
 let socket;
 
 const Chat = ({ location }) => {
+  const [senderId, setSenderId] = useState("");
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
   const [message, setMessage] = useState("");
@@ -18,20 +22,49 @@ const Chat = ({ location }) => {
   const ENDPOINT = "localhost:3001";
 
   useEffect(() => {
-    const { name, room } = queryString.parse(location.search);
+    const { senderId, name, room } = queryString.parse(location.search);
 
     socket = io(ENDPOINT);
 
+    setSenderId(Number(senderId));
     setName(name);
     setRoom(room);
 
-    socket.emit("join", { name, room }, () => {});
+    socket.emit("join", { senderId, name, room }, () => {});
 
     return () => {
       socket.emit("disconnect");
       socket.off();
     };
   }, [ENDPOINT, location.search]);
+
+  // Get Message List From Db
+  useEffect(() => {
+    const { room } = queryString.parse(location.search);
+    setRoom(room);
+
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        token: getFromStorage("the_main_app"),
+      },
+    };
+
+    fetch(
+      `http://localhost:3001/messages?conversationId=${room}`,
+      requestOptions
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.success) {
+          setMessages(json.data);
+        } else if (!json.success) {
+          console.error("No Message Found");
+        }
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
   useEffect(() => {
     socket.on("message", (message) => {
@@ -51,7 +84,7 @@ const Chat = ({ location }) => {
     <div className="outerContainer">
       <div className="container">
         <InfoBar room={room} />
-        <Messages messages={messages} name={name} />
+        <Messages messages={messages} senderId={senderId} senderName={name} />
         <Input
           message={message}
           setMessage={setMessage}
